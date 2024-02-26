313097919160e78f9d51394184b4990b57d03667
李江辰 <SpaceCode@foxmail.com>
2024-02-26 16:49:24 +0800

    chore: remove coverage badge and perf main

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,12 +1,35 @@
 import * as main from '../src/main'
+// import * as github from '@actions/github'
+import * as core from '@actions/core'
 
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+// Mock GitHub context
+jest.mock('@actions/github', () => ({
+  context: {
+    eventName: 'pull_request',
+    payload: {
+      pull_request: {
+        body: 'Test PR body'
+      }
+    }
+  }
+}))
 
-describe('index', () => {
+// Mock core functions
+jest.mock('@actions/core', () => ({
+  ...jest.requireActual('@actions/core'), // 保留其它功能
+  setFailed: jest.fn(),
+  info: jest.fn()
+}))
+
+describe('main', () => {
   it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+    const runSpy = jest.spyOn(main, 'run')
+    await main.run() // 直接调用 run 函数
+    expect(runSpy).toHaveBeenCalled()
+  })
 
-    expect(runMock).toHaveBeenCalled()
+  it('does not fail', async () => {
+    await main.run()
+    expect(core.setFailed).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import * as core from '@actions/core'
-// import { wait } from './wait'
+import * as github from '@actions/github'
+
+const context = github.context
 
 export async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('token')
+    // const owner = context.repo.owner;
+    // const repo = context.repo.repo;
+
+    if (context.eventName === 'pull_request') {
+      const body = context.payload.pull_request?.body
+      // 输出 body
+      console.log(body)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
